bd98871f1e1749abe3de3cfd15ed12599393f599
shoma <koalakoala.xxx@gmail.com>
2023-12-10 00:46:02 +0900

    Add Cognito user pool and S3 bucket for hosting static web site

--- cloudfront-fargate-terraform-template/frontend-resources.tf
+++ cloudfront-fargate-terraform-template/frontend-resources.tf
@@ -1,3 +1,4 @@
+#region: Cognito
 resource "aws_cognito_user_pool" "cognito-user-pool" {
   name = "${var.resource_name_prefix}-user-pool"
 
@@ -42,3 +43,121 @@ resource "aws_cognito_user_pool_client" "cognito-user-pool-clinet" {
   name         = "${var.resource_name_prefix}-user-pool-client"
   user_pool_id = aws_cognito_user_pool.cognito-user-pool.id
 }
+
+#endregion: Cognito
+
+#region: S3 bucket for hosting static web site
+
+resource "aws_s3_bucket" "bucket-for-hosting" {
+  bucket = "${var.resource_name_prefix}-frontend-bucket"
+}
+
+resource "aws_s3_bucket_ownership_controls" "bucket-for-hosting-ownership-controls" {
+  bucket = aws_s3_bucket.bucket-for-hosting.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket-for-hosting-acl" {
+  bucket     = aws_s3_bucket.bucket-for-hosting.id
+  acl        = "private"
+  depends_on = [aws_s3_bucket_ownership_controls.bucket-for-hosting-ownership-controls]
+}
+
+resource "aws_s3_bucket_website_configuration" "backend-for-hosting-website-configuration" {
+  bucket = aws_s3_bucket.bucket-for-hosting.id
+  index_document {
+    suffix = "index.html"
+  }
+
+  # When using React router, you need to set this property.
+  error_document {
+    key = "index.html"
+  }
+}
+
+
+data "aws_iam_policy_document" "bucket-for-hosting-policy" {
+  statement {
+    sid    = "Allow CloudFront access to S3 bucket"
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+    ]
+    resources = [
+      "${s3.bucket-for-hosting.arn}/*",
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_distribution.for-frontend.arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow-cloudfront-access" {
+  bucket = aws_s3_bucket.bucket-for-hosting.id
+  policy = data.aws_iam_policy_document.bucket-for-hosting-policy.json
+}
+
+
+#endregion: S3 bucket for hosting static web site
+
+#region: CloudFront
+
+resource "aws_cloudfront_distribution" "for-frontend" {
+  origin {
+    domain_name              = aws_s3_bucket.bucket-for-hosting.website_endpoint
+    origin_id                = aws_s3_bucket.bucket-for-hosting.id
+    origin_access_control_id = aws_cloudfront_origin_access_control.for-frontend.id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = aws_s3_bucket.bucket-for-hosting.id
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  # https://docs.aws.amazon.com/ja_jp/AmazonCloudFront/latest/DeveloperGuide/PriceClass.html
+  price_class = "PriceClass_100"
+
+  tags = {
+    Name = "${var.resource_name_prefix}-cloudfront"
+  }
+}
+
+resource "aws_cloudfront_origin_access_control" "for-frontend" {
+  name                              = "${var.resource_name_prefix}-cloudfront-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+#endregion: CloudFront
--- cloudfront-fargate-terraform-template/output.tf
+++ cloudfront-fargate-terraform-template/output.tf
@@ -5,3 +5,7 @@ output "user_pool_id" {
 output "user_pool_client_id" {
   value = aws_cognito_user_pool_client.cognito-user-pool-clinet.id
 }
+
+output "cloudfront_url" {
+  value = aws_cloudfront_distribution.for-frontend.domain_name
+}
